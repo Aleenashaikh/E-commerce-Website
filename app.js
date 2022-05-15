917bf4950b584a14d80b6ff53d888152b5577949
Aleenashaikh <AleeShaikh02@gmail.com>
2022-05-15 23:16:26 +0500

    Work done!

---
+++ app.js
@@ -0,0 +1,8 @@
+
+    const toggleButton = document.getElementById("toggle-Button");
+    const naviList = document.getElementById("navi-list");
+
+    toggleButton.addEventListener("click", () => {
+        naviList.classList.toggle("active");
+    })
+
